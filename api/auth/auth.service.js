6e15cf6cbe1fa7d9548917ecdb7f5e05273b1008
doronKudra <doron0505@gmail.com>
2025-12-30 20:10:02 +0200

    delete public

--- api/auth/auth.service.js
+++ api/auth/auth.service.js
@@ -4,7 +4,7 @@ import bcrypt from 'bcrypt'
 import { userService } from '../user/user.service.js'
 import { logger } from '../../services/logger.service.js'
 
-const cryptr = new Cryptr(process.env.SECRET)
+const cryptr = new Cryptr(process.env.SECRET || 'Secret-Puk-1234')
 
 export const authService = {
 	signup,
