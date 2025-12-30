e757a0e6f575673ebe2808a1471ff68685c60451
doronKudra <doron0505@gmail.com>
2025-12-30 19:46:13 +0200

    fix config

--- api/services/cloudinary.service.js
+++ api/services/cloudinary.service.js
@@ -1,5 +1,10 @@
+import { v2 as cloudinary } from 'cloudinary'
 
-import cloudinary from '../../config/cloudinary.config.js'
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_API_SECRET,
+})
 
 export function uploadStationImage(buffer, stationId) {
     return new Promise((resolve, reject) => {
---
+++ config/index.js
@@ -0,0 +1,17 @@
+import configProd from './prod.js'
+import configDev from './dev.js'
+
+
+export var config = configProd // always mongo-atlas
+
+// if (process.env.NODE_ENV === 'production') {
+//   config = configProd
+// } else {
+//   console.log('process.env.NODE_ENV :',process.env.NODE_ENV )
+//   config = configDev
+// }
+// config.isGuestMode = true
+
+
+//ASlAdWIGoDG3nPgU
+//mongodb+srv://zoharar7788_db_user:ASlAdWIGoDG3nPgU@oopsify.legpe7f.mongodb.net/?appName=oopsify
